02591517db95c311e2fc66cba9f98fe4b279ef74
melskrm <melissakerim20@gmail.com>
2025-07-24 23:02:17 +0300

    footer section eklendi

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
+/*faqs section start*/
 const accordion = document.getElementsByClassName("question");
 for (i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function () {
     this.classList.toggle("active");
   });
 }
+
+/*faqs section end*/
